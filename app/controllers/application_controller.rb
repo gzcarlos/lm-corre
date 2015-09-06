717c1f18b1d83736b3dd7cd7fe071acc2c599e3b
Carlos  Rosario <gozalito_128@hotmail.com>
2015-09-06 20:45:14 +0000

    Add place holder for Home Page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def home_placeholder
+    render text: "LM - Corre"
+  end
 end
